5f1e5e2de94e2684f64c44d6b6e20b7acebd849a
kwenealete <kwenait@yahoo.ca>
2021-10-29 05:46:31 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+export const endpoint = `https://beta.pokeapi.co/graphql/console/`;
+export const prodEndpoint = `fill me in when we deploy`;
+export const perPage = 4;
